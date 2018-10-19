8568ab43afbabf3e195dab8c3e7a62437ff2d92a
mathusummut <mathusum.mut@gmail.com>
2018-10-19 18:03:14 +0200

    PWA test

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,27 @@
-self.addEventListener("install",function(e){e.waitUntil(caches.open("feelingunlucky").then(function(e){return e.addAll(["/css/homepage.css","/js/trianglify.js","/js/confetti.js","/js/google-search.js","/js/jquery.js","/js/jquery-color.js","/js/main.js","/js/facebook-header.js","/js/twitter-share.js","/index.html","/favicon.ico"])}))}),self.addEventListener("fetch",function(e){e.respondWith(fetch(e.request).catch(function(){return caches.open("feelingunlucky").then(function(n){return n.match("/"+e.request.url.split("?")[0].replace(/^(?:\/\/|[^\/]+)*\//,""))})}))});self.addEventListener("install",function(e){e.waitUntil(caches.open("feelingunlucky").then(function(e){return e.addAll(["/css/homepage.css","/js/trianglify.js","/js/confetti.js","/js/google-search.js","/js/jquery.js","/js/jquery-color.js","/js/main.js","/js/facebook-header.js","/js/twitter-share.js","/index.html","/favicon.ico"])}))}),self.addEventListener("fetch",function(e){e.respondWith(fetch(e.request).catch(function(){return caches.open("feelingunlucky").then(function(n){return n.match("/"+e.request.url.split("?")[0].replace(/^(?:\/\/|[^\/]+)*\//,""))})}))});
+//Install stage sets up the offline page in the cache and opens a new cache
+self.addEventListener("install", function(event) {
+	event.waitUntil(caches.open("feelingunlucky").then(function(cache) {
+		return cache.addAll([
+			"/css/homepage.css",
+			"/js/trianglify.js",
+			"/js/confetti.js",
+			"/js/google-search.js",
+			"/js/jquery.js",
+			"/js/jquery-color.js",
+			"/js/main.js",
+			"/js/facebook-header.js",
+			"/js/twitter-share.js",
+			"/index.html",
+			"/favicon.ico"
+		]);
+	}));
+});
+
+self.addEventListener("fetch", function(event) {
+	event.respondWith(fetch(event.request).catch(function() {
+		return caches.open("feelingunlucky").then(function(cache) {
+			console.log("/" + event.request.url.split("?")[0].replace(/^(?:\/\/|[^\/]+)*\//, ""));
+			return cache.match("/" + event.request.url.split("?")[0].replace(/^(?:\/\/|[^\/]+)*\//, ""));
+		});
+	}));
+});
