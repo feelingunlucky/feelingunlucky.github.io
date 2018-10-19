812e182bf3e2818dc35b557100e7ed6ec6efd86c
mathusummut <mathusum.mut@gmail.com>
2018-10-19 15:52:56 +0200

    Minified service worker

--- register-sw.js
+++ register-sw.js
@@ -1,10 +1,1 @@
-if (navigator.serviceWorker.controller)
-	console.log('Active service worker found, no need to register')
-else {
-	//Register the ServiceWorker
-	navigator.serviceWorker.register('service-worker.js', {
-		scope: '/'
-	}).then(function(reg) {
-		console.log('Service worker has been registered for scope:'+ reg.scope);
-	});
-}
+navigator.serviceWorker.controller||navigator.serviceWorker.register("service-worker.js",{scope:"/"}).then(function(r){});
