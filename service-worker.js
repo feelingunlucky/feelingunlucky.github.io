5e7c067be2edda7521d273ee1e6c5453347de6bd
mathusummut <mathusum.mut@gmail.com>
2018-10-19 16:54:51 +0200

    PWA Chrome workaround

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,35 @@
-self.addEventListener("install",function(e){var n=new Request("/index.html");e.waitUntil(fetch(n).then(function(e){return caches.open("feelingunlucky-offline").then(function(t){return t.put(n,e)})}))}),self.addEventListener("fetch",function(e){"only-if-cached"===e.request.cache&&"same-origin"!==e.request.mode||e.respondWith(fetch(e.request).catch(function(e){return caches.open("feelingunlucky-offline").then(function(e){return e.match("/index.html")})}))}),self.addEventListener("refreshOffline",function(e){return caches.open("feelingunlucky-offline").then(function(n){return n.put(offlinePage,e)})});
+//Install stage sets up the offline page in the cache and opens a new cache
+self.addEventListener("install", function(event) {
+	event.waitUntil(caches.open("feelingunlucky").then(function(cache) {
+		return cache.addAll([
+			'/css/homepage.css',
+			'/js/trianglify.js',
+			'/js/confetti.js',
+			'/js/google-search.js',
+			'/js/jquery.js',
+			'/js/jquery-color.js',
+			'/js/main.js',
+			'/js/facebook-header.js',
+			'/js/twitter-share.js',
+			'/index.html'
+		]);
+	}));
+});
+/*//If any fetch fails, it will show the offline page
+self.addEventListener("fetch", function(event) {
+	if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin")
+		return;
+	event.respondWith(fetch(event.request).catch(function(error) {
+		return caches.open("home").then(function(cache) {
+			return cache.match("index.html");
+		});
+	}));
+});*/
+
+self.addEventListener("fetch", function(event) {
+	event.respondWith(fetch(event.request).catch(function() {
+		return caches.open("feelingunlucky").then(function(cache) {
+			return cache.match(event.request);
+		});
+	}));
+});
