fc6630a0a4d09f2b73a14d5889da5be8dbfb6b4e
mathusummut <mathusum.mut@gmail.com>
2018-10-19 15:45:09 +0200

    Test service worker

--- register-sw.js
+++ register-sw.js
@@ -2,7 +2,7 @@ if (navigator.serviceWorker.controller)
 	console.log('Active service worker found, no need to register')
 else {
 	//Register the ServiceWorker
-	navigator.serviceWorker.register('/js/service-worker.js', {
+	navigator.serviceWorker.register('service-worker.js', {
 		scope: '/'
 	}).then(function(reg) {
 		console.log('Service worker has been registered for scope:'+ reg.scope);
