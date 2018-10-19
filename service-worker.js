c2f6efc825ce145327ff8f137dbb74c890fd1b54
mathusummut <mathusum.mut@gmail.com>
2018-10-19 17:25:06 +0200

    PWA Chrome workaround

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,27 @@
-self.addEventListener("install",function(e){e.waitUntil(caches.open("feelingunlucky").then(function(e){return e.addAll(["/css/homepage.css","/js/trianglify.js","/js/confetti.js","/js/google-search.js","/js/jquery.js","/js/jquery-color.js","/js/main.js","/js/facebook-header.js","/js/twitter-share.js","/index.html","/favicon.ico"])}))}),self.addEventListener("fetch",function(e){e.respondWith(fetch(e.request).catch(function(){return caches.open("feelingunlucky").then(function(e){return e.match("/index.html")})}))});
+//Install stage sets up the offline page in the cache and opens a new cache
+self.addEventListener("install", function(event) {
+	event.waitUntil(caches.open("feelingunlucky").then(function(cache) {
+		return cache.addAll([
+			"/css/homepage.css",
+			"/js/trianglify.js",
+			"/js/confetti.js",
+			"/js/google-search.js",
+			"/js/jquery.js",
+			"/js/jquery-color.js",
+			"/js/main.js",
+			"/js/facebook-header.js",
+			"/js/twitter-share.js",
+			"/index.html",
+			"/favicon.ico"
+		]);
+	}));
+});
+
+self.addEventListener("fetch", function(event) {
+	event.respondWith(fetch(event.request).catch(function() {
+		console.log(JSON.stringify(event));
+		return caches.open("feelingunlucky").then(function(cache) {
+			return cache.match("/index.html");
+		});
+	}));
+});
